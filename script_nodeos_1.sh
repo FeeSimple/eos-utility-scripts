f83509da90fced5986e0efb905f98a502db43417
trung <trungtt1981@gmail.com>
2018-04-28 01:57:37 +0700

    Redirect log output to file

--- script_nodeos_1.sh
+++ script_nodeos_1.sh
@@ -5,5 +5,5 @@ base_dir=~/nodeos_data
 config_dir=./nodeos_config
 data_dir=$base_dir/$name
 
-nohup nodeos -e -p $name --config-dir $config_dir --data-dir $data_dir \
-    2>> $datadir/nodeos.log &
+nodeos -e -p $name --config-dir $config_dir --data-dir $data_dir \
+    > $data_dir/stdout.txt 2> $data_dir/stderr.txt &  echo $! > $data_dir/nodeos.pid
