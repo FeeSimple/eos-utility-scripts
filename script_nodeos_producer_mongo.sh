adbd89ee56c4a240f33221911e8d87fd9dda473d
trung <trungtt1981@gmail.com>
2018-05-13 23:04:14 +0700

    Add script involved with mongo

---
+++ script_nodeos_producer_mongo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Name must be eosio for running as block producer
+name=eosio
+base_dir=~/nodeos_data
+config_dir=./nodeos_config
+data_dir=$base_dir/$name
+
+nodeos -e -p $name --config-dir $config_dir --data-dir $data_dir --resync-blockchain 
