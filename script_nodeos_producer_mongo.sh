4e14f431b750412a1dbb431ac2b90eb7061a770b
trung <trungtt1981@gmail.com>
2018-05-13 23:14:14 +0700

    Add para for starting nodeos connected to a given mongodb

--- script_nodeos_producer_mongo.sh
+++ script_nodeos_producer_mongo.sh
@@ -6,4 +6,6 @@ base_dir=~/nodeos_data
 config_dir=./nodeos_config
 data_dir=$base_dir/$name
 
-nodeos -e -p $name --config-dir $config_dir --data-dir $data_dir --resync-blockchain 
+nodeos -e -p $name --config-dir $config_dir --data-dir $data_dir \
+  --resync-blockchain --plugin eosio::mongo_db_plugin \
+  --mongodb-uri mongodb://localhost:27017/EOStest
